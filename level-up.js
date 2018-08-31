3d0a3d1662ae2409ffecc3f0b91f206fc8192045
Jamie Ridding <jamie.ridding@icloud.com>
2018-08-31 03:26:53 +0100

    Re-implements rank roles.

--- level-up.js
+++ level-up.js
@@ -1,53 +1,40 @@
-//    TODO: re-implement
-//    // overcomplicated rank adding code
-//    if (__user.lvl >= 5){
-//      if (!message.member.roles.has("403125967939436545")){
-//        message.member.addRole("403125967939436545")
-//	message.author.send("You've ranked up to `NewCummer`!")
-//      }
-//    }
-//
-//    if (__user.lvl >= 10){
-//      if (!message.member.roles.has("403126021500567552")){
-//        message.member.addRole("403126021500567552")
-//	message.author.send("You've ranked up to `Daily Fapper`! \nYou can now use the command `/color`. ")
-//      }
-//    }
-//
-//    if (__user.lvl >= 30){
-//      if (!message.member.roles.has("403126248487780372")){
-//        message.member.addRole("403126248487780372")
-//	message.author.send("You've ranked up to `Addicted to Porn`!")
-//      }
-//    }
-//
-//    if (__user.lvl >= 60){
-//      if (!message.member.roles.has("403126385981521941")){
-//        message.member.addRole("403126385981521941")
-//	message.author.send("You've ranked up to `Pervert!`")
-//      }
-//    }
-//
-//    if (__user.lvl >= 100){
-//      if (!message.member.roles.has("403126466210037771")){
-//        message.member.addRole("403126466210037771")
-//	message.author.send("Holy shit, you've become a `God of Lewdness`.. This is the highest possible rank you can get!")
-//      }
-//    }
-//
-//
-//  __user.isModified()? __user.save() : void 0 ;
-
 const User = db.model("User");
 
 function callback(document) {
+  // Sorry Shy, but it runs quicker.
+  switch(true) {
+    case (document.lvl == 5):
+      this.member.addRole("403125967939436545").then(() => {
+        this.author.send("You've ranked up to `NewCummer`!");
+      });
+      break;
+    case (document.lvl == 10):
+      this.member.addRole("403126021500567552").then(() => {
+        this.author.send(["You've ranked up to `Daily Fapper`!", "You can now use the `/color` command."]);
+      });
+      break;
+    case (document.lvl == 30):
+      this.member.addRole("403126248487780372").then(() => {
+        this.author.send("You've ranked up to `Addicted to Porn`!");
+      });
+      break;
+    case (document.lvl == 60):
+      this.member.addRole("403126385981521941").then(() => {
+        this.author.send("You've ranked up to `Pervert`!");
+      });
+      break;
+    case (document.lvl == 100):
+      this.member.addRole("403126466210037771").then(() => {
+        this.author.send("Holy shit, you've become a `God of Lewdness`.. This is the highest possible rank you can get!");
+      });
+      break;
+  }
+
   if (document.exp > document.nxtlvl) {
     return User.findOneAndUpdate({ userId:this.author.id },
                                  { $inc: { lvl:1, nxtlvl:(document.lvl * 100 * 1.3) } },
                                  { "upsert":true, "setDefaultsOnInsert":true, "new":true }, bind(this));
   }
-
-
 };
 
 function bind(message) {
