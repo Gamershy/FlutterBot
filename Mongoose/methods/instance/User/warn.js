a8bf524b4487afc9dbf1a0051b80170b5bf6b17f
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2018-09-14 16:48:58 +0100

    Creates a method to allow for easier warning of users.

---
+++ Mongoose/methods/instance/User/warn.js
@@ -0,0 +1,29 @@
+function warn(issuer, reason) {
+  return new Promise((resolve, reject) => {
+    let user = this;
+
+    function userFindOneCallback(err, issuer) {
+      if (err) {
+        return reject(err);
+      }
+
+      db.model("Warn").create({
+        user: user._id,
+        issuer: issuer._id,
+        reason
+      }, warnCreateCallback);
+    }
+
+    function warnCreateCallback(err, warning) {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(warning);
+    }
+
+    db.model("User").findOne({userId: issuer.id}, "_id", userFindOneCallback);
+  });
+}
+
+module.exports = warn;
