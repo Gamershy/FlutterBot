6b3ddbd3345c15fab02a95a0d88c6ac8cab891af
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2018-09-14 16:49:56 +0100

    Creates a static method to simplify grabbing all of a user's warnings

---
+++ Mongoose/methods/static/Warn/forUser.js
@@ -0,0 +1,25 @@
+function forUser(userId, active = true, grabAll = false) {
+  return new Promise((resolve, reject) => {
+    function userFindOneCallback(err, user) {
+      if (err) return reject(err);
+
+      if (!user) {
+        return resolve(0, []);
+      }
+
+      let find = {user:user._id};
+
+      if (!grabAll) find.active = active;
+
+      db.model("Warn").find(find, warnFindCallback);
+    }
+
+    function warnFindCallback(err, warnings) {
+      if (err) return reject(err); else return resolve(warnings.length, warnings);
+    }
+
+    db.model("User").findOne({userId}, "_id", userFindOneCallback);
+  });
+}
+
+module.exports = forUser;
