9fbf0749881035aec7f32ee860ee365ff1b8dcfc
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2018-09-14 19:59:48 +0100

    Ensures Sirabot does not attempt to DM herself or other bots.

--- level-up.js
+++ level-up.js
@@ -4,28 +4,28 @@ function callback(document) {
   // Sorry Shy, but it runs quicker.
   switch(true) {
     case (document.lvl == 5):
-      this.member.addRole("403125967939436545").then(() => {
-        this.author.send("You've ranked up to `NewCummer`!");
+      if (!this.member.roles.has("403125967939436545")) this.member.addRole("403125967939436545").then(() => {
+        if (!this.author.bot) this.author.send("You've ranked up to `NewCummer`!");
       });
       break;
     case (document.lvl == 10):
-      this.member.addRole("403126021500567552").then(() => {
-        this.author.send(["You've ranked up to `Daily Fapper`!", "You can now use the `/color` command."]);
+      if (!this.member.roles.has("403126021500567552")) this.member.addRole("403126021500567552").then(() => {
+        if (!this.author.bot) this.author.send(["You've ranked up to `Daily Fapper`!", "You can now use the `/color` command."]);
       });
       break;
     case (document.lvl == 30):
-      this.member.addRole("403126248487780372").then(() => {
-        this.author.send("You've ranked up to `Addicted to Porn`!");
+      if (!this.member.roles.has("403126248487780372")) this.member.addRole("403126248487780372").then(() => {
+        if (!this.author.bot) this.author.send("You've ranked up to `Addicted to Porn`!");
       });
       break;
     case (document.lvl == 60):
-      this.member.addRole("403126385981521941").then(() => {
-        this.author.send("You've ranked up to `Pervert`!");
+      if (!this.member.roles.has("403126385981521941")) this.member.addRole("403126385981521941").then(() => {
+        if (!this.author.bot) this.author.send("You've ranked up to `Pervert`!");
       });
       break;
     case (document.lvl == 100):
-      this.member.addRole("403126466210037771").then(() => {
-        this.author.send("Holy shit, you've become a `God of Lewdness`.. This is the highest possible rank you can get!");
+      if (!this.member.roles.has("403126466210037771")) this.member.addRole("403126466210037771").then(() => {
+        if (!this.author.bot) this.author.send("Holy shit, you've become a `God of Lewdness`.. This is the highest possible rank you can get!");
       });
       break;
   }
