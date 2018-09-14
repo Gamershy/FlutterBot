012970c88f71e77bcad0dbdbb5207cd9a50ab3fb
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2018-09-14 14:55:34 +0100

    Fixes date calculation for Warn schema, allows Mongoose to calculate default instead of setting it to bot startup time

--- Mongoose/schemas/Warn.js
+++ Mongoose/schemas/Warn.js
@@ -3,8 +3,7 @@ const {Schema} = require("mongoose");
 var WarnSchema = new Schema({
   issuer:   {type: String, required: true},
   reason:   {type: String, required: true},
-  date:     {type: Date, default: Date.now()}
-
+  date:     {type: Date, default: Date.now}
 });
 
 module.exports = WarnSchema;
